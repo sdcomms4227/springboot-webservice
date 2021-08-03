9113f089028b1fca0d8e95b157c4313745dd467c
SDC <sdcomms4227@gmail.com>
2021-08-03 16:53:08 +0900

    4장 머스테치로 화면 구성하기

--- src/main/java/com/sdcomms/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/sdcomms/book/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.sdcomms.book.springboot.service.posts;
 
 import com.sdcomms.book.springboot.domain.posts.Posts;
 import com.sdcomms.book.springboot.domain.posts.PostsRepository;
+import com.sdcomms.book.springboot.web.dto.PostsListResponseDto;
 import com.sdcomms.book.springboot.web.dto.PostsResponseDto;
 import com.sdcomms.book.springboot.web.dto.PostsSaveRequestDto;
 import com.sdcomms.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,11 +32,25 @@ public class PostsService {
 
         return id;
     }
-
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts  = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
